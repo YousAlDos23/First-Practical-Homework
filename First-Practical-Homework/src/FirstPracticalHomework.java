f468a9a1bf38d519cd5cbe08166c628811ca0591
Josep <Josep@Youssef-msi>
2021-02-04 12:15:22 +0300

    First comment

---
+++ First-Practical-Homework/src/FirstPracticalHomework.java
@@ -0,0 +1,12 @@
+// This program is written by: Youssef Al-Dossari
+// Date: 21/02/04
+// Course name: 212CSS-4 Object Oriented Programming
+
+public class FirstPracticalHomework {
+	public static void main(String[] args) {
+		
+		System.out.println("Youssef Al-Dossari");
+
+	}
+
+}
